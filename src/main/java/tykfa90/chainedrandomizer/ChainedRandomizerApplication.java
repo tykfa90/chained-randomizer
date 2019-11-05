e599708f7ecba9525ccc702c4626f8e4f5685969
Konstanty Białek <tykfa90@gmail.com>
2019-11-05 12:14:11 +0100

    Initial commit

---
+++ src/main/java/tykfa90/chainedrandomizer/ChainedRandomizerApplication.java
@@ -0,0 +1,13 @@
+package tykfa90.chainedrandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainedRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainedRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tykfa90/chainedrandomizer/ChainedRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package tykfa90.chainedrandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainedRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
